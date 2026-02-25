671c65149888a91a889d8346cb2bb6efbdc120cc
Larissa <larisouzadev@gmail.com>
2026-02-25 10:29:24 -0300

    inicializando o projeto

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ToastContainer />
+      <BrowserRouter>
+      
+      
+      
+      
+      
+      
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
